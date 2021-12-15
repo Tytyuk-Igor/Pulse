105668beb015101310ce5a330b289309d78840d7
Tytyuk-Igor <igortytyuk@gmail.com>
2021-12-15 18:28:07 +0200

    Исправлены пути к файлам для компиляции

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
-const sass = require('gulp-sass')(require("sass"));
+const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
@@ -13,11 +13,10 @@ gulp.task('server', function () {
         }
     });
 
-    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('styles', function () {
-    return gulp.src("src/sass/*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.scss")
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(rename({ suffix: '.min', prefix: '' }))
         .pipe(autoprefixer())
@@ -27,7 +26,8 @@ gulp.task('styles', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/sass/**/*.scss)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 })
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
