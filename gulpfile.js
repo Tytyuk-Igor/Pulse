7ba0a5589170a2c25d9f34bd2938b46a554ab4dd
Tytyuk-Igor <igortytyuk@gmail.com>
2021-12-16 19:57:42 +0200

    Приведены в порядок файлы, сверстан первый экран

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ gulp.task('server', function () {
     });
 
 });
+gulp.watch("src/*.html").on('change', browserSync.reload);
 
 gulp.task('styles', function () {
     return gulp.src("src/sass/**/*.scss")
@@ -26,8 +27,7 @@ gulp.task('styles', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch("src/sass/**/*.scss)", gulp.parallel('styles'));
-    gulp.watch("src/*.html").on('change', browserSync.reload);
-})
+    gulp.watch("src/sass/**/*.scss", gulp.parallel('styles'));
+});
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
